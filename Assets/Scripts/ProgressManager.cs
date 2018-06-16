2566ac1c4dbb8ff10d950b41167d696dc393cbba
Maxime PIGEON <maxpige@gmail.com>
2018-06-16 18:54:27 +0200

    save view with timer

---
+++ Assets/Scripts/ProgressManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProgressManager : MonoBehaviour {
+
+    public UnityEngine.UI.Scrollbar progress;
+    public UnityEngine.UI.Text progressText;
+    public int scoreMax;
+    public int score;
+	// Use this for initialization
+	void Start () {
+        score = 0;
+
+        UpdateUI();
+	}
+
+    void UpdateScore(int n) {
+        score += n;
+        UpdateUI();
+    }
+
+    private void UpdateUI() {
+        progress.size = (float)score / (float)scoreMax;
+        progressText.text = score + " / " + scoreMax;
+    }
+}
